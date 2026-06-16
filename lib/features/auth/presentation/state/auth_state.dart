89d6cbef9984beaf2a69998815c4ec264df5fb83
vinayakstha <vsth005@gmail.com>
2026-06-16 19:04:02 +0545

    auth stauts

---
+++ lib/features/auth/presentation/state/auth_state.dart
@@ -0,0 +1,38 @@
+import 'package:client/features/auth/domain/entities/auth_entity.dart';
+import 'package:equatable/equatable.dart';
+
+enum AuthStatus {
+  initial,
+  loading,
+  authenticated,
+  unauthenticated,
+  registered,
+  error,
+}
+
+class AuthState extends Equatable {
+  final AuthStatus status;
+  final AuthEntity? authEntity;
+  final String? errorMessage;
+
+  const AuthState({
+    this.status = AuthStatus.initial,
+    this.authEntity,
+    this.errorMessage,
+  });
+
+  AuthState copyWith({
+    AuthStatus? status,
+    AuthEntity? authEntity,
+    String? errorMessage,
+  }) {
+    return AuthState(
+      status: status ?? this.status,
+      authEntity: authEntity ?? this.authEntity,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, authEntity, errorMessage];
+}
