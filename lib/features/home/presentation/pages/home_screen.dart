faf81c3fa7a6ba26772cd157584a2e3fe0725824
vinayakstha <vsth005@gmail.com>
2026-06-22 21:44:37 +0545

    home screen design

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -11,9 +11,68 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Padding(padding: EdgeInsets.all(16.0), child: CardWidget()),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // TOP BAR
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      children: [
+                        TextSpan(text: "get"),
+                        TextSpan(
+                          text: "my",
+                          style: TextStyle(color: Color(0xFF0464D4)),
+                        ),
+                        TextSpan(text: "phone"),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.notifications_none,
+                      color: Color(0xFF0464D4),
+                    ),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 16),
+
+              // SEARCH BAR
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.search),
+                    hintText: "Search for models, brands, or parts...",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              const CardWidget(),
+            ],
+          ),
+        ),
       ),
     );
   }
