5b28920f13fc942e5270aacc26c88d1755729d46
vinayakstha <vsth005@gmail.com>
2026-06-22 21:29:37 +0545

    card widget added in the home screen

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:client/features/home/presentation/widgets/card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(padding: EdgeInsets.all(16.0), child: CardWidget()),
+      ),
+    );
   }
 }
