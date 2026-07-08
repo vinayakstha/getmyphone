836a1590bffc1b4b1e1d7dcfbe5cf5f00124c11c
vinayakstha <vsth005@gmail.com>
2026-07-08 17:27:17 +0545

    navigation after splash screen changed

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -1,27 +1,37 @@
 import 'dart:async';
 
+import 'package:client/core/services/storage/user_session_service.dart';
+import 'package:client/features/nav/presentation/pages/navigation_screen.dart';
 import 'package:client/features/onboarding/presentation/pages/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
 
     Timer(const Duration(seconds: 3), () {
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const OnboardingScreen()),
-        );
-      }
+      if (!mounted) return;
+
+      final userSessionService = ref.read(userSessionServiceProvider);
+      final isLoggedIn = userSessionService.isLoggedIn();
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => isLoggedIn
+              ? const NavigationScreen()
+              : const OnboardingScreen(),
+        ),
+      );
     });
   }
 
